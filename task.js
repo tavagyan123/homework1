157cef5da27ed9d08218a18541d01aca66542370
tavagyan123 <85104894+tavagyan123@users.noreply.github.com>
2021-05-31 16:46:25 +0400

    Add files via upload

---
+++ task.js
@@ -0,0 +1,48 @@
+//1
+let name = "John"
+let surname = "Smith"
+console.log("Your name is" + name)
+console.log("Your surname is" + surname)
+
+//2
+let a = 5
+let b = 3
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+//3
+let a = 78656
+console.log(a%10)
+let a = -122
+console.log(a%10)
+let a =8
+console.log(a%10)
+let a = 0 
+console.log(a%10)
+
+//4
+let name = "Tigran"
+let surname = "Avagyan"
+console.log("My name is" + name  + surname); 
+
+//5
+
+let a=8
+let b=12
+
+a=a+b
+b=a-b
+a=a-b
+console.log(a,b);
+
+let a=10
+let b=20
+
+a=a+b
+b=a-b
+a=a-b
+console.log(a,b);
+
